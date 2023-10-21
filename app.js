c49630f89085929690770726fc698c81db0ca606
okashashuda <okashahuda@hotmail.com>
2023-10-21 13:39:31 -0700

    Co-authored-by: danielyk2007 <danielyk2007@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const lastLoginDate = localStorage.getItem('lastLoginDate');
-const todayLoginDate = localStorage.setItem('lastLoginDate', new Date().toString());
+// Retrieve lastLoginDate from localStorage and convert it to a Date object
+const lastLoginDate = new Date(localStorage.getItem('lastLoginDate'));
+
+// Get the current date
+const todayLoginDate = new Date();
+const dateString = todayLoginDate.toISOString();
+localStorage.setItem('lastLoginDate', dateString);
+
+// Calculate the time difference in milliseconds
+const timeDifference = todayLoginDate - lastLoginDate;
+
+// Convert the time difference to days
+const daysPassed = 1; //Math.floor(timeDifference / (1000 * 60));
+
+console.log("days passed " + daysPassed);
+
+function decayRateCalculator(daysPassed) {
+    //check if hyration less than 5, if so then it would be a linear decay rate
+    return ((1/50)*(daysPassed)*(daysPassed))+5
+}
 
 // Initial hydration and nutrient levels
 let hydration = 100;
 let nutrients = 100;
-
-// Decay rates per second
-
-//change decay rate so it decays exponentially
-const hydrationDecayRate = 0.1;
-const nutrientDecayRate = 0.25;
-
-
 
 // Get DOM elements
 const waterButton = document.getElementById("waterButton");
@@ -31,6 +41,41 @@ function updateDisplay() {
         clearInterval(nutrientInterval);
     }
 }
+
+//Diaster function, P(X) = 1/8, X = disaster happening
+// Check for a disaster event upon login
+function checkForDisaster() {
+    if (Math.random() < 0.125) { // 1/8 chance
+        alert("A disaster has occurred!");
+        
+        // Halve the hydration and nutrient levels
+        hydration *= 0.5;
+        nutrients *= 0.5;
+
+        // Update the display to reflect the new levels
+        updateDisplay();
+    }
+}
+
+// Call the checkForDisaster function when the user logs in
+checkForDisaster();
+
+// Decay functions
+function decayHydration() {
+    hydration -= decayRateCalculator(daysPassed);
+    if (hydration < 0) hydration = 0;
+    updateDisplay();
+}
+
+function decayNutrients() {
+    nutrients -= decayRateCalculator(daysPassed);
+    if (nutrients < 0) nutrients = 0;
+    updateDisplay();
+}
+
+//when opened use decayhydration and decay nutrients
+decayHydration();
+decayNutrients();
 
 //function to see if watering is possible 
 function canWater() {
@@ -62,18 +107,16 @@ waterButton.addEventListener('click', function() {
     if (canWater()) {
         // Logic for watering the plant
         // e.g., increase hydration level
-        console.log("Plant watered!");
+        alert("Plant watered!");
         localStorage.setItem('lastWatered', new Date().toString());
 
-        messageDiv.textContent = ''; // Clear any previous messages
-
         hydration += 1;
+        if (hydration > 100) hydration = 100;
 
         updateDisplay();
         
     } else {
-        console.log("You've already watered the plant today!");
-        messageDiv.textContent = "Can't water"; // Display the message on the screen
+        alert("You've already watered the plant today!");
     }
 });
 
@@ -82,8 +125,13 @@ mulchButton.addEventListener('click', function() {
     if (canMulch()) {
         // Logic for adding mulch
         // e.g., increase nutrient level
-        console.log("Mulch added!");
+        alert("Mulch added!");
         localStorage.setItem('lastMulched', new Date().toString());
+
+        if (Math.random() < 0.125) {
+            alert("Bonus mulch recived!");
+            nutrients += 10;
+        }
 
         nutrients += 1;
         if (nutrients > 100) nutrients = 100; 
@@ -91,29 +139,44 @@ mulchButton.addEventListener('click', function() {
         updateDisplay();
 
     } else {
-        console.log("You've already added mulch today!");
+        alert("You've already added mulch today!");
     }
 });
 
-// Decay functions
-function decayHydration() {
-    hydration -= hydrationDecayRate;
-    if (hydration < 0) hydration = 0;
-    updateDisplay();
+// New Leaf mechanic
+const newLeafMsg = document.getElementById("newLeaf");
+const submitButton = document.getElementById("submitMsg");
+
+if (daysPassed > 0) {
+    newLeafMsg.style.display = 'block';
+    submitButton.style.display = 'block';
+    
+    submitButton.addEventListener("click", function () {
+        const msg = newLeafMsg.value;
+
+        newLeafMsg.style.display = 'none';
+        submitButton.style.display = 'none';
+
+        const messageElement = document.getElementById('div');
+        messageElement.textContent = "Leaf written";
+        document.body.appendChild(messageElement);
+        
+        setTimeout(function () {
+            document.body.removeChild(doneMsg);
+        }, 5000);
+    });
 }
 
-function decayNutrients() {
-    nutrients -= nutrientDecayRate;
-    if (nutrients < 0) nutrients = 0;
-    updateDisplay();
-}
+//splash screen
+document.addEventListener("DOMContentLoaded", function () {
+    const splashScreen = document.getElementById("splashScreen");
 
-// Set intervals for decay
-const hydrationInterval = setInterval(decayHydration, 1000); // Decrease hydration every second
-const nutrientInterval = setInterval(decayNutrients, 1000); // Decrease nutrients every second
+    // Show the splash screen when the page loads
+    splashScreen.style.display = "flex";
 
-const lastLogin = new Date(lastLoginDate);
-const timeDifference = todayLoginDate - lastLoginDate;
-const daysPassed = Math.floor(timeDifference / (1000*60*60*24));
+    splashScreen.addEventListener("click", function () {
+        // Hide the splash screen when it's clicked
+        splashScreen.style.display = "none";
+    });
+});
 
-console.log("Days since last login: " + daysPassed);
